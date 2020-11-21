9d48310347a5f2380eeaeead5c54fc3ae620412d
yaptt <yaptingting29@gmail.com>
2020-11-21 17:26:56 +0800

    add route for login,get user profile and update user

--- server.js
+++ server.js
@@ -7,14 +7,16 @@ const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const userController = require("./controllers/userController");
+const userModel = require("./models/userModel");
+
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+const {DB_USER, DB_PASS, DB_HOST, DB_NAME} = process.env;
 const mongoURI = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}`;
 mongoose.set("useFindAndModify", false);
-
 app.get("/api/v1", (req, res) => {
   res.status(200).json({
     success: true,
@@ -22,8 +24,18 @@ app.get("/api/v1", (req, res) => {
   });
 });
 
+// =======================================
+//              ROUTES
+// =======================================
+
+app.post("/api/v1/user/register", userController.registerUser); // registration post
+app.post("/api/v1/user/login", userController.userLogin); // login post
+// app.post("/api/v1/user/login", userController.userLogout); // logout post
+app.get("/api/v1/users/:id", userController.userProfile); // get user profile
+app.patch("/api/v1/user/:id", userController.updateUser); // update route
+
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoURI, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => {
     console.log("DB connection successful");
 
